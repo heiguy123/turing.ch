8cae594d5f61aeb044fd27f1001c8ce307929063
Chai1237 <hjie2003@gmail.com>
2021-10-02 01:37:30 +0800

    no message

--- app/screen/CalculatorPage.js
+++ app/screen/CalculatorPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, StatusBar, Platform, SafeAreaView, Image } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, StatusBar, Platform, SafeAreaView, Image, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -23,18 +23,87 @@ function ChooseScreen({ navigation: { navigate } }) {
         </View>
     );
 }
-function DefCal(props) {
+
+function DefCal({navigation}) {
+
     return (
-        <View style={{ flex : 1, alignItems:"center", justifyContent: "center" }}>
-            <Text>Hello World</Text>
+        <View style={Calculation.container}>
+            <Image resizeMode={"contain"} style={{alignSelf:"flex-start", left:"6%", top:"6%", width:"2%"}} source={require('./assets/arrow.png')}/>
+            <Text style={Calculation.h1} onPress={()=> navigation.goBack() }>Back</Text>
+            <Text style={[Calculation.textBase, {left:"6%", top:"8%"}]}>Panel Specification</Text>
+            <Text style={[Calculation.textBase1,{left:"6%", top:"10%",}]}>Specification 1</Text>
+            <View style={[Calculation.textBox, {top:"23%", width:"90%", height: "5%"}]}>
+                <Text style={{alignSelf:"center", left:"30%"}}  >Power at Mpp : 435 Watt</Text>
+            </View>
+            <Text style={[Calculation.textBase1,{left:"6%", top:"18%",}]}>Specification 2</Text>
+            <Text style={[Calculation.textBase1,{left:"55%", top:"15.5%",}]}>Specification 3</Text>
+            <View style={[Calculation.textBox, {left: "5%" , top:"34%", width:"40%", height: "5%"}]}>
+                <Text style={{alignSelf:"center", left:"30%"}} >Area: 2 m^2</Text>
+            </View>
+            <View style={[Calculation.textBox, {left: "55%" , top:"34%", width:"40%", height: "5%"}]}>
+                <Text style={{alignSelf:"center", left:"30%"}} >Used time: 10 years</Text>
+            </View>
+            <Text style={[Calculation.textBase, {left:"6%", top:"27%"}]}>Electricity Settings</Text>
+            <Text style={[Calculation.textBase1,{left:"6%", top:"29.5%",}]}>Specification 1</Text>
+            <View style={[Calculation.textBox, {left: "5%" , top:"55%", width:"90%", height: "5%"}]}>
+                <TextInput
+                    placeholder="Electricity price per kWh in your area (US Dollar)" 
+                    style={{alignSelf:"center", left:"30%"}} />
+            </View>
+            <Text style={[Calculation.textBase1,{left:"6%", top:"38%",}]}>Specification 2</Text>
+            <View style={[Calculation.textBox, {left: "5%" , top:"66%", width:"90%", height: "5%"}]}>
+                <TextInput  
+                    placeholder="Estimate your electricity bill per month (US Dollar)" 
+                    style={{alignSelf:"center", left:"30%"}} />
+            </View>
+            <TouchableOpacity style={Calculation.DoneBut} onPress={() => console.log('CalCulation Time!!')}>
+                <Text style={{alignSelf:"center", color:"#fff", fontSize:16, fontWeight:"normal"}}>Done</Text>
+            </TouchableOpacity>
         </View>
     );
 }
 
-function CusCal(props) {
+
+function CusCal({navigation : {navigate}}) {
     return (
-        <View style={{ flex : 1, alignItems:"center", justifyContent: "center" }}>
-            <Text>Hello World</Text>
+        <View style={Calculation.container}>
+            <Image resizeMode={"contain"} style={{alignSelf:"flex-start", left:"6%", top:"6%", width:"2%"}} source={require('./assets/arrow.png')}/>
+            <Text style={Calculation.h1} onPress={()=> navigate('Choose') }>Back</Text>
+            <Text style={[Calculation.textBase, {left:"6%", top:"8%"}]}>Panel Specification</Text>
+            <Text style={[Calculation.textBase1,{left:"6%", top:"10%",}]}>Specification 1</Text>
+            <View style={[Calculation.textBox, {top:"23%", width:"90%", height: "5%"}]}>
+                <TextInput  
+                    placeholder="Power at Maximum Power Point (MPP)" 
+                    style={{alignSelf:"center", left:"30%"}} />
+            </View>
+            <Text style={[Calculation.textBase1,{left:"6%", top:"18%",}]}>Specification 2</Text>
+            <Text style={[Calculation.textBase1,{left:"55%", top:"15.5%",}]}>Specification 3</Text>
+            <View style={[Calculation.textBox, {left: "5%" , top:"34%", width:"40%", height: "5%"}]}>
+                <TextInput  
+                    placeholder="Area of panels (m^2)" 
+                    style={{alignSelf:"center", left:"30%"}} />
+            </View>
+            <View style={[Calculation.textBox, {left: "55%" , top:"34%", width:"40%", height: "5%"}]}>
+                <TextInput  
+                    placeholder="Used time (Year)" 
+                    style={{alignSelf:"center", left:"30%"}} />
+            </View>
+            <Text style={[Calculation.textBase, {left:"6%", top:"27%"}]}>Electricity Settings</Text>
+            <Text style={[Calculation.textBase1,{left:"6%", top:"29.5%",}]}>Specification 1</Text>
+            <View style={[Calculation.textBox, {left: "5%" , top:"55%", width:"90%", height: "5%"}]}>
+                <TextInput  
+                    placeholder="Electricity price per kWh in your area (US Dollar)" 
+                    style={{alignSelf:"center", left:"30%"}} />
+            </View>
+            <Text style={[Calculation.textBase1,{left:"6%", top:"38%",}]}>Specification 2</Text>
+            <View style={[Calculation.textBox, {left: "5%" , top:"66%", width:"90%", height: "5%"}]}>
+                <TextInput  
+                    placeholder="Estimate your electricity bill per month (US Dollar)" 
+                    style={{alignSelf:"center", left:"30%"}} />
+            </View>
+            <TouchableOpacity style={Calculation.DoneBut} onPress={() => console.log('CalCulation Time!!')}>
+                <Text style={{alignSelf:"center", color:"#fff", fontSize:16, fontWeight:"normal"}}>Done</Text>
+            </TouchableOpacity>
         </View>
     );
 }
@@ -58,6 +127,53 @@ function CalculatorPage() {
 
 
 export default CalculatorPage;
+
+const Calculation = StyleSheet.create({
+    container:{
+        backgroundColor:"#fff",
+        flex:1,
+        alignItems:"center",
+    },
+    textBase:{
+        fontSize: 24,
+        fontWeight: "bold",
+        alignSelf: "flex-start",
+        color: "#000",
+    },
+    textBase1:{
+        fontSize: 14,
+        color:"#555555",
+        fontWeight: "normal",
+        alignSelf: "flex-start",
+    },
+    DoneBut:{
+        backgroundColor:"#233E8B",
+        borderRadius:16,
+        position:"absolute",
+        top: "77%",
+        height:"5.5%",
+        width: "65%",
+        alignSelf:"center",
+        justifyContent:"center",
+    },
+    textBox:{
+        flexDirection:"row",
+        backgroundColor: "#ECECEC",
+        borderRadius: 6,
+        position: "absolute",
+        alignSelf: "center",
+    },
+    h1: {
+        fontWeight: "bold",
+        fontSize: 18,
+        color: "black",
+        alignSelf: "flex-start",
+        top: "3%",
+        left: "10%",
+        letterSpacing: 0.15,
+      },
+
+})
 
 const styles = StyleSheet.create({
     container:{
@@ -132,5 +248,4 @@ const styles = StyleSheet.create({
         lineHeight:20,
         color:"#233E8B"
     },
-    
 })
--- app/screen/DefCal.js
+++ app/screen/DefCal.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+// export default function App() {
+//     return (
+//     <SafeAreaView style={styles.container}>
+//         <View style={styles.container1}>
+//             <View>
+//                 <Text style={styles.h1}>
+//                     Back
+//                 </Text>
+//             </View>
+//         </View>
+//     </SafeAreaView>
+//     );
+// }
 
-function DefCal(props) {
-    return (
-        <View style={{ flex : 1, alignItems:"center", justifyContent: "center" }}>
-            <Text>Hello World</Text>
-        </View>
-    );
-}
 
-export default DefCal;
+// const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       backgroundColor: "#fff",
+//       paddingTop: Platform.OS === "android"? StatusBar.currentHeight : 0,
+//     },
+//     container1: {
+//       width: "90%",
+//     },
+//     h1: {
+//       fontWeight: "bold",
+//       fontSize: 18,
+//       color: "black",
+//       textAlign: "left",
+//       letterSpacing: 0.15,
+//     },
+// });
