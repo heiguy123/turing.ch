0a3d55f891af6f56ee42c71b5b03452bc40efd24
Jeremy0326 <90554469+Jeremy0326@users.noreply.github.com>
2021-10-01 22:41:41 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, Text, View, StatusBar, Platform } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, View, Image, StatusBar, Platform } from 'react-native';
 
 export default function App() {
     return (
     <SafeAreaView style={styles.container}>
         <View style={styles.container1}>
             <View>
-                <Text style={styles.h1}>
-                    Back
+              <View style={{flexDirection: "row", alignItems: "center"}}>
+                <Image style={styles.image}
+                source={require("./assets/back.png")}/>
+                <Text style={styles.title1}>
+                    Back.
                 </Text>
+              </View>
+              <Text style={styles.title2}>
+                Panel Specification
+              </Text>
+              <Text style={styles.title3}>
+                Specification 1
+              </Text>
+              <Text style={styles.title2}>
+                Electricity Settings
+              </Text>
+              <Text style={styles.title3}>
+                Specification 2
+              </Text>
             </View>
         </View>
     </SafeAreaView>
@@ -23,11 +39,30 @@ const styles = StyleSheet.create({
       paddingTop: Platform.OS === "android"? StatusBar.currentHeight : 0,
     },
     container1: {
-      width: "90%",
+      marginHorizontal: "8%",
+      paddingTop: "8%",
     },
-    h1: {
+    image: {
+      width: 20,
+      height: 20,
+    },
+    title1: {
       fontWeight: "bold",
       fontSize: 18,
+      color: "black",
+      textAlign: "left",
+      letterSpacing: 0.15,
+    },
+    title2: {
+      fontWeight: "bold",
+      fontSize: 24,
+      color: "black",
+      textAlign: "left",
+      letterSpacing: 0.15,
+    },
+    title3: {
+      fontWeight: "normal",
+      fontSize: 14,
       color: "black",
       textAlign: "left",
       letterSpacing: 0.15,
